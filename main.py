67c814badb314c5f462efb7ac1ad76ec0f5e3201
Ryan Raasch <ryanraasch94@gmail.com>
2023-10-16 22:04:46 +0000

    fixing main()

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 
 def main():
   print("Hello! This is main.")
-  return json.dumps("message": "SUCCESS")
+  return json.dumps({"message": "SUCCESS"})
 
 if __name__ == "__main__":
   main()
