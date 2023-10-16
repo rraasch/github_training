1ddb56ab3bc7e8ff67d8d3612379b7736cb48d6d
Ryan Raasch <ryanraasch94@gmail.com>
2023-10-16 22:15:48 +0000

    adjusted indentation

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import json
-import os
+
 
 def main():
-  print("Hello! This is main.")
-  return json.dumps({"message": "SUCCESS"})
+    print("Hello! This is main.")
+    return json.dumps({"message": "SUCCESS"})
 
 if __name__ == "__main__":
-  main()
+    main()
